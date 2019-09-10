c0c8f7e0ddb60f38a0e28f7b7ed7e53407e92b50
NIlsSchnitzel <235770@student.insulacollege.nl>
2019-09-10 12:26:31 +0200

    Java_Script.3

---
+++ b1w2l3.js
@@ -0,0 +1,14 @@
+alert(' Hallo Rompie tafels neef back in groep 3 ');  
+var e=3
+var y=6
+
+for (i = 1; i <=10; i++){
+ document.write('<br/>' +i+'x'+y+ '= ' + (y*i))
+}
+
+document.write('<br/>')
+
+for (i = 1; i <=10; i++){
+	document.write('<br/>' +i+'x'+ e + '= ' + (e*i))
+}
+document.write('<br/><br/> 6+12x10/5-12= ' + (6+12*10/5-12))
